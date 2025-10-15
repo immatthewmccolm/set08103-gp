d15e3da84ddd76a77335603f2bceab1a445f9e74
immatthewmccolm <40712747@live.napier.ac.uk>
2025-10-15 23:50:03 +0100

    Added menu and formatting functionality as well as creating helper methods like getUserInput

--- src/main/java/com/napier/gp/App.java
+++ src/main/java/com/napier/gp/App.java
@@ -1,8 +1,35 @@
-package com.napier.gp;
+/*
+ * App.java
+ * Main Class File used to run the program.
+ */
 
+package com.napier.gp;
+import static com.napier.gp.Menu.*;
+import static com.napier.gp.UserInput.*;
+
+
+/**
+ * Main class used to run the program.
+ */
 public class App {
-    public static void main(String[] args)
-    {
-        System.out.println("Hello World!");
+    public static void main(String[] args) {
+        // Prints Page Title
+        pageTitle("Country Data Analysis Software");
+
+        // Prints Menu Items
+        menuItem(1, "All Countries (Ordered by Population)");
+
+        // Gets user input and loads the appropriate method, otherwise loops until a valid input is received
+        do {
+            String menuInput = getUserInput("Enter your menu choice");
+
+            if (menuInput.equals("1")) {
+                // call method to run report
+                break; // exit the loop after a valid option
+            } else {
+                System.out.println("Please enter a valid menu option.\n");
+            }
+
+        } while (true);
     }
 }
---
+++ src/main/java/com/napier/gp/UserInput.java
@@ -0,0 +1,25 @@
+/*
+* userInput.java
+* Class file for handling User Input within the programme
+*/
+package com.napier.gp;
+
+import java.util.Scanner;
+
+/**
+ * Class for handling code relating to user inputs
+ */
+public class UserInput {
+    // Single Scanner shared across all calls
+    private static final Scanner scanner = new Scanner(System.in);
+
+    /**
+     * Takes a prompt as a string and returns the input from the console as a string
+     * @param prompt Question that is displayed
+     * @return User Input from Console
+     */
+    public static String getUserInput(String prompt) {
+        System.out.print(prompt + ": ");
+        return scanner.nextLine();
+    }
+}
