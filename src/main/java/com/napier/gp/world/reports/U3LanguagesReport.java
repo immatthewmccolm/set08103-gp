9437555f7f4cfab51562e2016c3d6eca50a8574c
immatthewmccolm <40712747@live.napier.ac.uk>
2025-11-24 15:45:53 +0000

    Updated

--- src/main/java/com/napier/gp/world/reports/U3LanguagesReport.java
+++ src/main/java/com/napier/gp/world/reports/U3LanguagesReport.java
@@ -9,52 +9,52 @@ import java.util.*;
 public class U3LanguagesReport {
 
     //
-//    public static void print() {
-//
-//        HashMap<String, Long> allLanguagePopulations = getWorldwideLanguageSpeakers();
-//        HashMap<String, Long> requiredLanguagePopulations = new HashMap<>();
-//
-//        requiredLanguagePopulations.put("Chinese", allLanguagePopulations.get("Chinese"));
-//        requiredLanguagePopulations.put("English", allLanguagePopulations.get("English"));
-//        requiredLanguagePopulations.put("Hindi", allLanguagePopulations.get("Hindi"));
-//        requiredLanguagePopulations.put("Spanish", allLanguagePopulations.get("Spanish"));
-//        requiredLanguagePopulations.put("Arabic", allLanguagePopulations.get("Arabic"));
-//
-//        // ALTER U1 METHOD TO RETURN AND NOT PRINT. ALTER ALL TO HAVE SEPARATE PRINT AND VALUE METHODS
-//        long worldPopulation = 0;
-//
-//        for(Country country : World.getInstance().getCountries()) {
-//            worldPopulation += country.getPopulation();
-//        }
-//        // world pop end
-//
-//        List<Map.Entry<String, Double>> percentages = getWorldwideLanguageSpeakersPercentages();
-//
-//        for(Map.Entry<String, Long> entry : requiredLanguagePopulations.entrySet()) {
-//            percentages.add(new AbstractMap.SimpleEntry<String, Double>(entry.getKey(), (double)Math.round((double)entry.getValue() / (double)worldPopulation * 1000) / 10));
-//        }
-//
-//        Map.Entry<String, Double> temp;
-//
-//        for(int i = 0; i < percentages.size() - 1; i++) {
-//            for(int j = 0; j < percentages.size() - i - 1; j++) {
-//                if(percentages.get(j).getValue() < percentages.get(j + 1).getValue()) {
-//                    temp = percentages.get(j);
-//                    percentages.set(j, percentages.get(j + 1));
-//                    percentages.set(j + 1, temp);
-//                }
-//            }
-//        }
-//
-//        for(Map.Entry<String, Double> entry : percentages) {
-//            for(Map.Entry<String, Long> entry2 : requiredLanguagePopulations.entrySet()) {
-//                if(entry2.getKey().equals(entry.getKey())) {
-//                    System.out.println(entry.getKey() + " " + entry2.getValue() + " " + entry.getValue() + "%");
-//                }
-//            }
-//        }
-//
-//    }
+    public static void print() {
+
+        HashMap<String, Long> allLanguagePopulations = getWorldwideLanguageSpeakers();
+        HashMap<String, Long> requiredLanguagePopulations = new HashMap<>();
+
+        requiredLanguagePopulations.put("Chinese", allLanguagePopulations.get("Chinese"));
+        requiredLanguagePopulations.put("English", allLanguagePopulations.get("English"));
+        requiredLanguagePopulations.put("Hindi", allLanguagePopulations.get("Hindi"));
+        requiredLanguagePopulations.put("Spanish", allLanguagePopulations.get("Spanish"));
+        requiredLanguagePopulations.put("Arabic", allLanguagePopulations.get("Arabic"));
+
+        // ALTER U1 METHOD TO RETURN AND NOT PRINT. ALTER ALL TO HAVE SEPARATE PRINT AND VALUE METHODS
+        long worldPopulation = 0;
+
+        for(Country country : World.getInstance().getCountries()) {
+            worldPopulation += country.getPopulation();
+        }
+        // world pop end
+
+        List<Map.Entry<String, Double>> percentages = getWorldwideLanguageSpeakersPercentages();
+
+        for(Map.Entry<String, Long> entry : requiredLanguagePopulations.entrySet()) {
+            percentages.add(new AbstractMap.SimpleEntry<String, Double>(entry.getKey(), (double)Math.round((double)entry.getValue() / (double)worldPopulation * 1000) / 10));
+        }
+
+        Map.Entry<String, Double> temp;
+
+        for(int i = 0; i < percentages.size() - 1; i++) {
+            for(int j = 0; j < percentages.size() - i - 1; j++) {
+                if(percentages.get(j).getValue() < percentages.get(j + 1).getValue()) {
+                    temp = percentages.get(j);
+                    percentages.set(j, percentages.get(j + 1));
+                    percentages.set(j + 1, temp);
+                }
+            }
+        }
+
+        for(Map.Entry<String, Double> entry : percentages) {
+            for(Map.Entry<String, Long> entry2 : requiredLanguagePopulations.entrySet()) {
+                if(entry2.getKey().equals(entry.getKey())) {
+                    System.out.println(entry.getKey() + " " + entry2.getValue() + " " + entry.getValue() + "%");
+                }
+            }
+        }
+
+    }
 
     //
     public static List<Map.Entry<String, Double>>getWorldwideLanguageSpeakersPercentages() {
